6a30054bee4d1de460f336c7b7ab53af91e633af
Oleksandr Miahkyi <89033562+AleksandrMyagkiy@users.noreply.github.com>
2026-09-07 22:02:05 +0300

    Bump NZT 365 to v4.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.nzt365.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "4.2.0"
+        versionCode = 20
+        versionName = "4.3.0"
     }
 
     signingConfigs {

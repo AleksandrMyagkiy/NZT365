77c5922bca5c0cb68ae708263455d292661238d4
Oleksandr Miahkyi <89033562+AleksandrMyagkiy@users.noreply.github.com>
2026-09-11 07:26:20 +0300

    Bump NZT 365 to v8.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.nzt365.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 23
-        versionName = "7.0.0"
+        versionCode = 24
+        versionName = "8.0.0"
     }
 
     signingConfigs {

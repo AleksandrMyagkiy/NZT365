4de60950bef538632c431210127d55e70d469cd7
Oleksandr Miahkyi <89033562+AleksandrMyagkiy@users.noreply.github.com>
2026-09-12 11:09:21 +0300

    Bump NZT 365 to v12.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.nzt365.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 27
-        versionName = "11.0.0"
+        versionCode = 28
+        versionName = "12.0.0"
     }
 
     signingConfigs {
